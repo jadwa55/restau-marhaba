ebf8ce01cdc34e8cdf0da950005b446862ab4044
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2022-03-30 15:00:51 +0000

    Configure MySQL database & Sequelize

---
+++ config/db.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "123456",
+    DB: "restodb",
+    dialect: "mysql",
+
+
+    // pool: {
+    //   max: 5,
+    //   min: 0,
+    //   acquire: 30000,
+    //   idle: 10000
+    // }
+  };
