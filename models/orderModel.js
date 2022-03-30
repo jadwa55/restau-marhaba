ec29f38fde0e755e020e848c65a336a7b5286ce8
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2022-03-30 15:19:29 +0000

    Create OrderModel

---
+++ models/orderModel.js
@@ -0,0 +1,36 @@
+/* -------------------------------------------------------------------------- */
+/*                               Order Model                              */
+/* -------------------------------------------------------------------------- */
+
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const sequelize = require('../config/db')
+
+
+class Order extends Model{}
+
+Order.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+
+    status: {
+        type: DataTypes.STRING,
+    },
+
+    address: {
+        type: DataTypes.STRING,
+        allowNull: false
+
+    }
+},
+{
+    sequelize: sequelize,
+    modelName: 'Order'
+})
+
+
+
+
+module.exports = Order;
