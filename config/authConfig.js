bb471130f78d3adc774b907162062d2a2d36c1a7
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2022-03-31 11:46:37 +0000

    create middleware function / verifySingUp

---
+++ config/authConfig.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "jadwa-secret-key"
+  };
--- server.js
+++ server.js
@@ -12,6 +12,30 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+
+const db = require("./app/models");
+const Role = db.role;
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync Db');
+  initial();
+});
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
+ 
+  Role.create({
+    id: 2,
+    name: "moderator"
+  });
+ 
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}
+
 // simple route
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to RESTOURANT MARHABA." });
