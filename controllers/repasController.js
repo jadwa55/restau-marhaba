f768e3e7d937deee4523353ff18f9319f47e6f6d
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2022-04-03 13:10:27 +0000

    storage image

--- controllers/repasController.js
+++ controllers/repasController.js
@@ -1,2 +1,13 @@
 const Repas = require('../models/repasModel')
 const multer = require('multer')
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/images')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+    })
+exports.upload = multer({ storage: storage })
