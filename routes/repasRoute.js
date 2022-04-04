ee2c17bb6242c6a7f280277306cfa057fa5ea738
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2022-04-04 10:28:03 +0000

    Delete repas route

--- routes/repasRoute.js
+++ routes/repasRoute.js
@@ -2,11 +2,17 @@ const Repas = require('../models/repasModel')
 const express = require('express')
 const router = express.Router()
 const repasController = require('../controllers/repasController')
-
+const { upload } = require('../controllers/repasController')
 router
     .route('/create')
-    .post(repasController.addRepas)
+    .post(upload.single("image"), repasController.addRepas)
 
 router
     .route('/update/:id')
     .post(repasController.updateRepas)
+
+router
+    .route('/delete/:id')
+    .delete(repasController.deleteRepas)
+
+module.exports = router;
