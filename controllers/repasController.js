0e7bb0ea60ef857469d35f65ee6f2b1f803e8f4c
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2022-04-04 09:57:23 +0000

    Create repas  Controller

--- controllers/repasController.js
+++ controllers/repasController.js
@@ -11,3 +11,27 @@ const storage = multer.diskStorage({
     }
     })
 exports.upload = multer({ storage: storage })
+
+exports.addRepas = async (req,res)=>{
+    try {
+
+        const {name,description,prix} = req.body
+        const image = req.file.path
+    
+        const repas = await Repas.create({
+            name: name,
+            image: image,
+            description: description,
+            prix: prix
+        })
+
+        res.status(200).json({
+            message: 'repas created successfully',
+            repas: repas
+        })
+        
+    } catch (error) {
+        res.send(error)
+    }
+
+}
