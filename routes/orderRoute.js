42ace5b828be17531496353b90d205e3da0673b8
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2022-04-04 12:39:39 +0000

    Create Order route

---
+++ routes/orderRoute.js
@@ -0,0 +1,8 @@
+const Order = require('../models/orderModel')
+const express = require('express')
+const router = express.Router()
+const orderController = require('../controllers/OrderController')
+
+router
+    .route('/create')
+    .post(orderController.addOrder)
