18f8bcc2930dc342eb635db851196704829cb8e6
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2022-04-05 09:47:22 +0000

    Create Order controller

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -10,13 +10,14 @@ exports.addOrder= async (req,res)=>{
     }
 
     const order= await Order.create({
-        type: type
+        type: type,
+        status: status,
+        address: address
     })
 
     res.status(200).json({
         message: 'Orderadded successfully',
         order: order
     })
-
-
 }
+
